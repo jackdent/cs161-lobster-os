08b20da34da9e61bf4f31afdfce31bda3d1c17e0
GRAHAM LUSTIBER <GRAHAMLUSTIBER@COLLEGE.HARVARD.EDU>
2016-02-25 08:21:52 -0500

    Take out testing code

--- kern/syscall/getpid.c
+++ kern/syscall/getpid.c
@@ -8,9 +8,8 @@
 int
 sys_getpid(pid_t* retval)
 {
-	struct proc* blah = curproc;
-	lock_acquire(blah->p_lock);
-	*retval = blah->p_pid;
-	lock_release(blah->p_lock);
+	lock_acquire(curproc->p_lock);
+	*retval = curproc->p_pid;
+	lock_release(curproc->p_lock);
 	return 0;
 }
