f7f47cc2c8b6eb2f23a5ac509f239feff43911b0
Jack Dent <jdent@college.harvard.edu>
2016-04-18 22:54:51 -0400

    Changes to record schema

--- kern/fs/sfs/sfs_record.h
+++ kern/fs/sfs/sfs_record.h
@@ -8,70 +8,43 @@
  * Record Schema
  */
 
-// Record types
-
 enum sfs_record_type {
         R_TX_BEGIN,
         R_TX_COMMIT,
 
         R_INODE_CAPTURE,
-        R_INODE_UPDATE,
+        R_INODE_RESIZE,
+        R_INODE_UPDATE_SLOT,
         R_INODE_RELEASE,
 
         R_DIRECTORY_ADD,
         R_DIRECTORY_REMOVE,
 
-        R_BLOCK_WRITE,
-
         R_FREEMAP_CAPTURE,
-        R_FREEMAP_RELEASE
+        R_FREEMAP_RELEASE,
+
+        R_USER_BLOCK_WRITE
 };
 
-// inode records
-
-struct sfs_inode_capture {
-        uint32_t ino;
-};
-
-// May need to split this up into link/unlink,
-// increase/decrease size, and add/remove block
-struct sfs_inode_update {
-        uint32_t ino;
-        struct sfs_dinode old_inode;
-        struct sfs_dinode new_inode;
-};
-
-struct sfs_inode_release {
-        uint32_t ino;
-        struct sfs_dinode old_inode;
-};
-
-// Directory records
-
-// adding or removing
-struct sfs_directory {
+struct sfs_update_directory {
         uint32_t parent_ino;
         int slot;
         uint32_t ino;
+        char name[SFS_NAMELEN];
 };
 
-// Block records
+struct sfs_meta_update_word {
+        daddr_t block;
+        off_t pos;
+        uint32_t old_value;
+        uint32_t new_value;
+};
 
-struct sfs_block_write {
+struct sfs_user_block_write {
         daddr_t block;
         off_t pos;
         size_t len;
         uint32_t checksum;
-};
-
-// Freemap records
-
-struct sfs_freemap_capture {
-        uint32_t block;
-};
-
-struct sfs_freemap_release {
-        uint32_t block;
 };
 
 // Journal record (directly serialized)
@@ -80,13 +53,9 @@ struct sfs_record {
         txid_t r_txid;
         enum sfs_record_type r_type;
         union {
-                struct sfs_inode_capture inode_capture;
-                struct sfs_inode_update inode_update;
-                struct sfs_inode_release inode_release;
-                struct sfs_directory directory;
-                struct sfs_block_write block_write;
-                struct sfs_freemap_capture freemap_capture;
-                struct sfs_freemap_release freemap_release;
+                struct sfs_update_directory update_directory;
+                struct sfs_meta_update_word meta_update_word;
+                struct sfs_user_block_write userblock_write;
         } r_parameters;
 };
 
@@ -102,5 +71,5 @@ struct sfs_record *sfs_create_commit_record(void);
  * Recovery operations
  */
 
-void sfs_record_undo(struct sfs_record record, enum sfs_record_type);
-void sfs_record_redo(struct sfs_record record, enum sfs_record_type);
+void sfs_record_undo(struct sfs_record, enum sfs_record_type);
+void sfs_record_redo(struct sfs_record, enum sfs_record_type);
